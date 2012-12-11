192cecb4a67e0888bdda92795749d7c004e4c2fe
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-12-11 01:03:29 -0700

    Updated AssignmentDetailFragment.java to remove calls to and use of dummy data. WIP, will finish after updating views so that calls can be made to their elements

--- EncourageWork/src/csci498/csmyth/encouragework/AssignmentDetailFragment.java
+++ EncourageWork/src/csci498/csmyth/encouragework/AssignmentDetailFragment.java
@@ -33,11 +33,6 @@ public class AssignmentDetailFragment extends Fragment {
     public static final String ARG_ITEM_ID = "item_id";
 
     /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
-
-    /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
@@ -48,24 +43,34 @@ public class AssignmentDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
+        /*if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
             mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-        }
+        }*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_assignment_detail, container, false);
+        /*View rootView = inflater.inflate(R.layout.fragment_assignment_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.assignment_detail)).setText(mItem.content);
         }
 
-        return rootView;
+        return rootView;*/
+    	return inflater.inflate(R.layout.fragment_assignment_detail, container, false);
     }
+    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+    	super.onActivityCreated(savedInstanceState);
+    	
+    	name = (EditText)getView().findViewById(R.id.name);
+    	// stopped here
+    }
+    
 }
