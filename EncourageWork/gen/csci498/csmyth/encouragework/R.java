baf633810350424fedef4d5485c170745b051018
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-12-10 20:49:19 -0700

    Added supports-screens to manifest

--- EncourageWork/gen/csci498/csmyth/encouragework/R.java
+++ EncourageWork/gen/csci498/csmyth/encouragework/R.java
@@ -41,6 +41,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int asmnt_name_field=0x7f040002;
         public static final int title_assignment_detail=0x7f040001;
     }
     public static final class style {
