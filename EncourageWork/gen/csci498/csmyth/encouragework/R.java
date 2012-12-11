d980c2059e8cae2fe6f29b19e657116d5391686b
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-12-10 21:30:58 -0700

    Reboot: starting over in favor of starting from scratch

--- EncourageWork/gen/csci498/csmyth/encouragework/R.java
+++ EncourageWork/gen/csci498/csmyth/encouragework/R.java
@@ -14,35 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int assignment_detail=0x7f060002;
-        public static final int assignment_detail_container=0x7f060000;
-        public static final int assignment_list=0x7f060001;
+        public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_assignment_detail=0x7f030000;
-        /** 
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-         */
-        public static final int activity_assignment_list=0x7f030001;
-        public static final int activity_assignment_twopane=0x7f030002;
-        public static final int fragment_assignment_detail=0x7f030003;
+        public static final int activity_encourage_work=0x7f030000;
+    }
+    public static final class menu {
+        public static final int activity_encourage_work=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int asmnt_name_field=0x7f040002;
-        public static final int title_assignment_detail=0x7f040001;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
     }
     public static final class style {
         /** 
