b8172f9766b593f4d2024153dc20f1bc9fe59cc6
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-12-11 05:24:36 -0700

    Removed two pane related code from AssignmentDetailActivity.java. Left Up button methods

--- EncourageWork/src/csci498/csmyth/encouragework/AssignmentDetailActivity.java
+++ EncourageWork/src/csci498/csmyth/encouragework/AssignmentDetailActivity.java
@@ -6,15 +6,6 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
-/**
- * An activity representing a single Assignment detail screen. This
- * activity is only used on handset devices. On tablet-size devices,
- * item details are presented side-by-side with a list of items
- * in a {@link AssignmentListActivity}.
- * <p>
- * This activity is mostly just a 'shell' activity containing nothing
- * more than a {@link AssignmentDetailFragment}.
- */
 public class AssignmentDetailActivity extends FragmentActivity {
 
     @Override
@@ -24,28 +15,6 @@ public class AssignmentDetailActivity extends FragmentActivity {
 
         // Show the Up button in the action bar.
         getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
-        if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(AssignmentListActivity.ID_EXTRA,
-                    getIntent().getStringExtra(AssignmentListActivity.ID_EXTRA));
-            AssignmentDetailFragment fragment = new AssignmentDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.assignment_detail_container, fragment)
-                    .commit();
-        }
     }
 
     @Override
