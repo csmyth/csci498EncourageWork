f1c47344c72ab1dbca77ae9792c2a44295ef0bb6
Cecilia Suderman <csmyth@mymail.mines.edu>
2012-12-11 05:55:36 -0700

    row.xml revamped. Cleaner design, now includes due date

--- EncourageWork/gen/csci498/csmyth/encouragework/R.java
+++ EncourageWork/gen/csci498/csmyth/encouragework/R.java
@@ -15,39 +15,24 @@ public final class R {
         public static final int ic_menu_add=0x7f020001;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f070007;
-        public static final int add=0x7f07000a;
+        public static final int add=0x7f07000b;
         public static final int assignment_detail_container=0x7f070000;
         public static final int assignment_list=0x7f070001;
-        public static final int check_complete=0x7f070009;
+        public static final int check_complete=0x7f07000a;
         public static final int complete=0x7f070004;
         public static final int date_picker=0x7f070005;
         public static final int details=0x7f070002;
+        public static final int due_date_row=0x7f070009;
+        public static final int due_tag=0x7f070008;
         public static final int name=0x7f070003;
         public static final int notes=0x7f070006;
-        public static final int title=0x7f070008;
+        public static final int title=0x7f070007;
     }
     public static final class layout {
         public static final int activity_assignment_detail=0x7f030000;
-        /** 
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-
-    Layout alias to replace the single-pane version of the layout with a
-    two-pane version on Large screens.
-
-    For more on layout aliases, see:
-    http://developer.android.com/training/multiscreen/screensizes.html#TaskUseAliasFilters
-    
-         */
         public static final int activity_assignment_list=0x7f030001;
-        public static final int activity_assignment_twopane=0x7f030002;
-        public static final int fragment_assignment_detail=0x7f030003;
-        public static final int row=0x7f030004;
+        public static final int fragment_assignment_detail=0x7f030002;
+        public static final int row=0x7f030003;
     }
     public static final class menu {
         public static final int option=0x7f060000;
@@ -56,6 +41,7 @@ public final class R {
         public static final int add_title=0x7f040004;
         public static final int app_name=0x7f040000;
         public static final int assignment_name_field=0x7f040002;
+        public static final int due_tag=0x7f040005;
         public static final int notes_hint=0x7f040003;
         public static final int title_assignment_detail=0x7f040001;
     }
